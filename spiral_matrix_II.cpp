6573ca5b1bfa2a909c7b67696afdf06c07eeba13
SwordSongs <xiaoding_hupo@foxmail.com>
2023-08-21 08:50:54 +0800

    spiral matrix commit

--- spiral_matrix_II.cpp
+++ spiral_matrix_II.cpp
@@ -3,37 +3,43 @@ public:
     vector<vector<int>> generateMatrix(int n) {
         int count = 1;
         int startX = 0, startY = 0;
-        int offset = 1;
-        int cycNum = n/2;
+        int offset = 1, cycNum = n/2;
         int j = startX, i = startY;
 
         vector<vector<int>> result;
-        result.resize(n*n);
-        
-        while(cycNum){
-            for(;j<n-offset;++j){
-                result[startX][j] = count++;
+        result.resize(n);
+
+        for (int i = 0; i < n; ++i) {
+            result[i].resize(n);
+        }
+
+        while (cycNum) {
+            j = startX, i = startY;
+
+            for (; j < n - offset; ++j) {
+                result[startY][j] = count++;
             }
 
-            for(;i<n-offset;++i){
+            for (; i < n - offset; ++i) {
                 result[i][j] = count++;
             }
 
-            for(;j>startX;--j){
+            for (; j > startX; --j) {
                 result[i][j] = count++;
             }
 
-            for(;i>startY;--i){
+            for (; i > startY; --i) {
                 result[i][j] = count++;
             }
-            
+
             ++startX;
             ++startY;
             ++offset;
             --cycNum;
         }
 
-        if(n%2 == 1){
+        if (n % 2 == 1) {
+            j = startX, i = startY;
             result[i][j] = count++;
         }
 
